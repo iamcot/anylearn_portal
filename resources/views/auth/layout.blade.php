cac08782170ae7e063d4730825c7ff2fbbdb9725
thang.truong <thang.truong@lazada.com>
2020-05-30 00:48:39 +0700

    favicon

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -9,7 +9,7 @@
     <meta name="author" content="">
 
     <title>{{ env('APP_NAME_LONG') }}</title>
-
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <!-- Custom fonts for this template-->
     <link href="/cdn/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
