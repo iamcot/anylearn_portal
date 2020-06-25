1e5077d6633e2cad18781d39bf021c5c40b4897f
thang.truong <thang.truong@lazada.com>
2020-06-25 13:50:45 +0700

    fix tag

--- resources/views/home/course.blade.php
+++ resources/views/home/course.blade.php
@@ -1,16 +1,13 @@
 <section class="blog_section big_padding" id="blog">
-    <div class="container">
-        <!-- <p class="default_text raleway text-center default_small_heading blue_color font_200">Hãy khám phá</p> -->
-        <h2 class="default_section_heading text-center text-green">
-            Nhiều khóa học trong các lĩnh vực
-        </h2>
-        <hr class="default_divider default_divider_blue default_divider_big">
-        <div class="blog_slider_main swiper-container">
-            <ul class="course-tags">
-                @foreach(config('home_courses', []) as $course)
-                <li>{{ $course }}</li>
-                @endforeach
-            </ul>
-        </div>
+    <h2 class="default_section_heading text-center text-green">
+        Nhiều khóa học trong các lĩnh vực
+    </h2>
+    <hr class="default_divider default_divider_blue default_divider_big">
+    <div class="blog_slider_main swiper-container">
+        <ul class="course-tags">
+            @foreach(config('home_courses', []) as $course)
+            <li>{{ $course }}</li>
+            @endforeach
+        </ul>
     </div>
 </section>
