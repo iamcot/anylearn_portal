c12cba9a3a1bbbe85c88ebd02e7dee16375f2d94
thang.truong <thang.truong@lazada.com>
2020-07-09 12:04:23 +0700

    fix homepage descript

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="anyLEARN.vn - HỌC không giới hạn, HỌC cho chính bạn">
+    <meta name="description" content="anyLEARN.vn - Nền tảng kết nối hàng đầu về giáo dục">
     <meta name="author" content="">
     <!-- Insert Favicon Here -->
     <link href="/favicon-16x16.png" rel="icon">
