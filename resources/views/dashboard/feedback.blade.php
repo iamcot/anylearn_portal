6b7ac373ed0d5434a89baea2a4c06dbf57d7ad1d
thang.truong <thang.truong@lazada.com>
2020-07-14 23:45:46 +0700

    feedback

--- resources/views/dashboard/feedback.blade.php
+++ resources/views/dashboard/feedback.blade.php
@@ -2,7 +2,7 @@
 @section('body')
 @if(sizeof($feedbacks) == 0)
 <div class="text-center mt-5 pt-5">
-    Chưa có phản hồi nào 
+    Chưa có phản hồi nào
 </div>
 @else
 <div class="card shadow">
@@ -22,7 +22,7 @@
                     <th class="text-center">{{ $feedback->id }}</th>
                     <td>{{ $feedback->user->name }}</td>
                     <td>{{ $feedback->content }}</td>
-                    <td><a target="_blank" href="{{ $feedback->file }}"><img style="height: 50px;" src="{{ $feedback->file }}" alt=""></a></td>
+                    <td><img class="view-image" data-url="{{ $feedback->file }}" style="height: 50px;" src="{{ $feedback->file }}" alt=""></td>
                     <td>{{ $feedback->created_at }}</td>
                 </tr>
                 @endforeach
@@ -34,7 +34,28 @@
         {{ $feedbacks->links() }}
     </div>
 </div>
+<div id="imageModal" class="modal fade shadow" tabindex="-1" role="dialog">
+    <div class="modal-dialog ">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title m-0 font-weight-bold text-{{ env('MAIN_COLOR', 'primary') }}">Ảnh chụp màn hình</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+            </div>
+            <div class="modal-body">
+            </div>
+        </div>
+    </div>
+</div>
 
 @endif
 
+@endsection
+@section('jscript')
+<script>
+    $('.view-image').click(function() {
+        var url = $(this).data("url");
+        $('#imageModal').find('.modal-body').html('<img style="" src="' + url + '">');
+        $('#imageModal').modal('show');
+    });
+</script>
 @endsection
