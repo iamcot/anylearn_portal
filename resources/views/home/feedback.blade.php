f9e0b032cab926922f5e50f709e811ae742cd803
thang.truong <thang.truong@lazada.com>
2021-03-16 17:11:00 +0700

    feedback avatar

--- resources/views/home/feedback.blade.php
+++ resources/views/home/feedback.blade.php
@@ -11,30 +11,35 @@
                         <p class="customer_feedback_text white_color open_sans default_text font_200">
                             Tôi cảm thấy rất vui khi biết đến app anyLEARN. Nhờ app mà khi đăng ký khóa học cho con, tôi đều được hoàn 1 khoản tiền để có thể rút ra hoặc dùng đăng ký cho con học các khóa học hay và bổ ích khác. Tôi rất chú trọng vào việc đầu tư học tập cho con nên cứ như vậy là tôi thấy rất thích.
                         </p>
+                        <p class="feedback_avatar"><img src="/cdn/onepage/images/feedbacks/kimchi.jpg" alt=""></p>
                         <p class="customer_feedback_name white_color open_sans default_small_heading font_600">Ms. Kim Chi – Phụ huynh</p>
                     </div>
                     <div class="swiper-slide">
                         <p class="customer_feedback_text white_color open_sans default_text font_200">
                             Sau khi con học khóa học Tư duy MMS được đăng ký trên app anyLEARN thì con về nhà thay đổi rất tích cực. Thông qua app anyLEARN mà mình mới biết đến khóa học hay như vậy để cho con cái của mình học.
                         </p>
+                        <p class="feedback_avatar"><img src="/cdn/onepage/images/feedbacks/kimpham.jpg" alt=""></p>
                         <p class="customer_feedback_name white_color open_sans default_small_heading font_600">Ms. Kim Phạm – Phụ huynh</p>
                     </div>
                     <div class="swiper-slide">
                         <p class="customer_feedback_text white_color open_sans default_text font_200">
                             Các trường học và Trung tâm đào tạo trên app anyLEARN đều đã được sàng lọc kỹ lưỡng nên tôi vô cùng yên tâm về chất lượng của các khóa học trên này. Từ học sinh, sinh viên, nhân viên văn phòng cho đến doanh nhân,.. tất cả mọi người đều có thể lên app để tìm được các khóa học phù hợp với mình.
                         </p>
+                        <p class="feedback_avatar"><img src="/cdn/onepage/images/feedbacks/xuanhoa.jpg" alt=""></p>
                         <p class="customer_feedback_name white_color open_sans default_small_heading font_600">Mr. Xuân Hòa – Phụ huynh</p>
                     </div>
                     <div class="swiper-slide">
                         <p class="customer_feedback_text white_color open_sans default_text font_200">
                             Cảm ơn anyLEARN đã mang lại cho mình nhiều khoá học bổ ích, nhờ có anyLEARN, mình đã học được nhiều kiến thức trong việc dạy con và cách bồi dưỡng, vun đắp tình cảm gia đình.
                         </p>
+                        <p class="feedback_avatar"><img src="/cdn/onepage/images/feedbacks/quynhnhu.jpg" alt=""></p>
                         <p class="customer_feedback_name white_color open_sans default_small_heading font_600">Quỳnh Như – Thành viên anyLEARN</p>
                     </div>
                     <div class="swiper-slide">
                         <p class="customer_feedback_text white_color open_sans default_text font_200">
                             Cảm ơn MC Hoài Trinh đã mang lại cho em khoá học bổ ích về Nghệ thuật toả sáng trước công chúng, giờ đây em đã không còn rụt rè, ngại ngùng mà đã tự tin hơn khi nói chuyện, thuyết trình trước đám đông. Xin chân thành cảm ơn chị.
                         </p>
+                        <p class="feedback_avatar"><img src="/cdn/onepage/images/feedbacks/ngochuyen.jpg" alt=""></p>
                         <p class="customer_feedback_name white_color open_sans default_small_heading font_600">Ngọc Huyền – Thành viên anyLEARN</p>
                     </div>
                 </div>
