540f78be9a69d3a71de1a7d8e877b77dec9f32de
thang.truong <thang.truong@lazada.com>
2021-04-23 09:59:06 +0700

    change bank

--- app/Http/Controllers/Apis/ConfigApi.php
+++ app/Http/Controllers/Apis/ConfigApi.php
@@ -87,9 +87,9 @@ class ConfigApi extends Controller
             'vip_days' => 30,
             'bank' => [
                 'bank_name' => 'Ngân hàng Quốc Tế VIB',
-                'bank_no' => '618704060057326',
-                'bank_branch' => 'CN Tân Định',
-                'account_name' => 'ĐẶNG THỊ HOÀI TRINH',
+                'bank_no' => '002159817',
+                'bank_branch' => 'CN Hoàng Văn Thái',
+                'account_name' => 'Công ty cổ phần Đầu Tư và Giáo dục anyLEARN',
                 'content' => 'Nap tien anyLEARN',
             ],
             'suggest_columns' => 3,
