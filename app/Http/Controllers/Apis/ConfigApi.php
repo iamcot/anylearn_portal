8313a0c6c72cd074809b2c3ff966226b7ec7a836
thang.truong <thang.truong@lazada.com>
2021-04-27 23:14:20 +0700

    fix search

--- app/Http/Controllers/Apis/ConfigApi.php
+++ app/Http/Controllers/Apis/ConfigApi.php
@@ -228,7 +228,7 @@ class ConfigApi extends Controller
                 ->where('items.user_status', '>', 0)
                 ->join('users', 'users.id', '=', 'items.user_id');
 
-                if (strpos("#", $query) == 0) {
+                if (strpos($query, "#") !== false) {
                     $tag = substr($query, 1);
                     $query = $query->where('items.tags', 'like', "%$tag%");
                 } else {
