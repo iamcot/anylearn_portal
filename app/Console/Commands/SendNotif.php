3cad08e080e73b2b0a9b0d4cd215b162cbc35627
thang.truong <thang.truong@lazada.com>
2021-09-03 13:12:42 +0700

    add try catch

--- app/Console/Commands/SendNotif.php
+++ app/Console/Commands/SendNotif.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Constants\NotifConstants;
 use App\Models\Notification;
 use App\Models\User;
+use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\DB;
@@ -43,23 +44,28 @@ class SendNotif extends Command
     public function handle()
     {
         // $notSendNotifs = Notification::where('is_send', 0)->get();
-        $userIds = $this->argument('userIds');
-        $message = $this->argument('message');
-        $type = $this->option('type');
-        $route = $this->option('route');
-        $copy = $this->option('copy');
-
-        $notifM = new Notification();
-        if ($type == 'phone') {
-            $users = User::whereIn('phone', explode(",", $userIds))->get();
-        } else {
-            $users = User::whereIn('id', explode(",", $userIds))->get();
+        try {
+            $userIds = $this->argument('userIds');
+            $message = $this->argument('message');
+            $type = $this->option('type');
+            $route = $this->option('route');
+            $copy = $this->option('copy');
+    
+            $notifM = new Notification();
+            if ($type == 'phone') {
+                $users = User::whereIn('phone', explode(",", $userIds))->get();
+            } else {
+                $users = User::whereIn('id', explode(",", $userIds))->get();
+            }
+            
+            foreach($users as $user) {
+                $notifM->createNotif(NotifConstants::SYSTEM_NOTIF, $user->id, ['message' => $message], $copy, $route);
+                print("Sent message to user " . $user->id . " | " .  $user->phone . " | " . $user->name . "\n");
+            }
+            print("--All done! --\n");
+        } catch (Exception $e) {
+            print($e);
         }
         
-        foreach($users as $user) {
-            $notifM->createNotif(NotifConstants::SYSTEM_NOTIF, $user->id, ['message' => $message], $copy, $route);
-            print("Sent message to user " . $user->id . " | " .  $user->phone . " | " . $user->name . "\n");
-        }
-        print("--All done! --\n");
     }
 }
