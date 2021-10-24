5f36d55948fdce49a2573c8517f8d73bc47c015e
thang.truong <thang.truong@lazada.com>
2021-10-24 13:04:35 +0700

    fix article

--- resources/views/anylearn/home/promotions.blade.php
+++ resources/views/anylearn/home/promotions.blade.php
@@ -1,16 +1,17 @@
 @if(!empty($promotions) && count($promotions) > 0)
+@inject('itemServ','App\Services\ItemServices')
 <section class="carousel3">
     <div id="{{ $carouselId }}" class="mx-auto my-auto">
         <h5 class="m-2 fw-bold text-uppercase">{{ $title }}</h5>
         <div class="owl-carousel owl-theme">
             @foreach($promotions as $promotion)
             <div class="p-1">
-                <div class="card border-0">
+                <a class="card border-0 text-decoration-none text-secondary" href="{{ $itemServ->articleUrl($promotion->id) }}">
                     <div class="card-img">
                         <img src="{{ $promotion->image }}" class="img-fluid">
                         <div class="promotion-tag">{{ $promotion->video }}</div>
                     </div>
-                </div>
+                </a>
             </div>
             @endforeach
         </div>
