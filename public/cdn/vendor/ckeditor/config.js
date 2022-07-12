37ada51602792798a8462587a07ba54e0b31a833
thang.truong <thang.truong@lazada.com>
2022-07-12 21:51:36 +0700

    support link

--- public/cdn/vendor/ckeditor/config.js
+++ public/cdn/vendor/ckeditor/config.js
@@ -3,39 +3,40 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For complete reference see:
-	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+CKEDITOR.editorConfig = function(config) {
+    // Define changes to default configuration here.
+    // For complete reference see:
+    // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
 
-	// The toolbar groups arrangement, optimized for two toolbar rows.
-	config.toolbarGroups = [
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'colors' },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-	];
+    // The toolbar groups arrangement, optimized for two toolbar rows.
+    config.toolbarGroups = [
+        { name: 'document', groups: ['mode', 'document', 'doctools'] },
+        { name: 'clipboard', groups: ['clipboard', 'undo'] },
+        { name: 'insert' },
+        { name: 'forms' },
+        { name: 'tools' },
+        { groups: ['links'] },
+        '/',
+        { name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+        { name: 'colors' },
+        { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi'] },
+        { name: 'styles' },
+    ];
 
-	// Remove some buttons provided by the standard plugins, which are
-	// not needed in the Standard(s) toolbar.
-	// config.removeButtons = 'Underline,Subscript,Superscript';
+    // Remove some buttons provided by the standard plugins, which are
+    // not needed in the Standard(s) toolbar.
+    // config.removeButtons = 'Underline,Subscript,Superscript';
 
-	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
+    // Set the most common block elements.
+    config.format_tags = 'p;h1;h2;h3;pre';
 
-	// Simplify the dialog windows.
+    // Simplify the dialog windows.
     config.removeDialogTabs = 'image:advanced;link:advanced';
-    
+
     config.removeDialogTabs = 'image:advanced;link:advanced';
     config.height = "500px";
     config.extraPlugins = 'simage';
+    config.extraPlugins = 'link';
     config.imageUploadURL = "/admin/upload/ckimage";
     config.extraAllowedContent = '*(*)';
     config.allowedContent = true;
