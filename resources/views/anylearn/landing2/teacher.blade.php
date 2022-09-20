cfad8afba085682b34d07d9df910b5730140653a
Quangnkts <quangnkts1908037@fpt.edu.vn>
2022-09-20 16:46:37 +0700

    AL-36 <update page info>

--- resources/views/anylearn/landing2/teacher.blade.php
+++ resources/views/anylearn/landing2/teacher.blade.php
@@ -3,14 +3,17 @@
         <div id="carouselExampleInterval" class="carousel slide carousel-fade" data-bs-ride="carousel">
             <div class="carousel-inner">
                 @foreach ($teachers as $teacher)
-                    <div class="carousel-item <?php if($teachers[0] == $teacher) echo'active' ?>" data-bs-interval="2000">
+                    <div class="carousel-item <?php if($teachers[0] == $teacher) echo'active' ?>" data-bs-interval="3000">
                         <div class="row">
                             <div class="col-sm-6">
                                 <div style="position: relative;"
-                                    class="d-none d-lg-block review-avatar-wrapper bg-success rounded p-2 text-white text-end">
+                                    class="d-none d-lg-block review-bcv-wrapper bg-success rounded p-2 text-white text-end">
                                     <div class="review-bcv mt-5">
-                                        <img src="{{ $teacher->image }}" class="d-block img-fluid imagebox bcv"
+                                        <img src="{{ $teacher->image }}" class="d-block img-cus imagebox bcv"
                                             alt="...">
+                                            {{-- <div class="img-responsive-wrap">
+                                                <a href="yourlink" class="img-inner" style="background-image: url('{{ $teacher->image }}');"><img alt="Image Alt" title="Your Title" src=""></a>
+                                            </div> --}}
                                         <div class="vector">
                                             <img src="./cdn/anylearn/img/Vector.png" class="d-block img-fluid"
                                                 alt="...">
@@ -21,7 +24,7 @@
                                     <div class="fw-bold text-start p-5 text-title"> Ban cố vấn & <br> chuyên gia</div>
                                     <div class="content-bcv text-start ">
                                         <p class="text-white">
-                                        <h5>{{ $teacher->name }}</h5>{{ $teacher->introduce }}</p>
+                                        <h5>{{ $teacher->name }}</h5>{{ substr($teacher->introduce,0,120) }}</p>
                                         <a href="/ref/anylearn?role=teacher"
                                             class="btn border rounded rounded-pill border-white btn btn-outline-success fs-6 fw-bold ps-4 pe-4 float-end mb-5 text-white">ĐĂNG
                                             KÝ LÀM CHUYÊN GIA <i class="fas fa-arrow-right"></i></a>
