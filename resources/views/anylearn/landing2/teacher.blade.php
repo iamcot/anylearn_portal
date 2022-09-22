e717724ff94a2bcb9afd3d18b2aa9c7c8a4a220a
Quangnkts <quangnkts1908037@fpt.edu.vn>
2022-09-22 13:39:03 +0700

    AL-36 <fix info pàge>

--- resources/views/anylearn/landing2/teacher.blade.php
+++ resources/views/anylearn/landing2/teacher.blade.php
@@ -5,30 +5,31 @@
                 @foreach ($teachers as $teacher)
                     <div class="carousel-item <?php if($teachers[0] == $teacher) echo'active' ?>" data-bs-interval="3000">
                         <div class="row">
-                            <div class="col-sm-6">
+                            
+                            <div class="col-sm-7">
                                 <div style="position: relative;"
-                                    class="d-none d-lg-block review-bcv-wrapper bg-success rounded p-2 text-white text-end">
+                                    class="review-bcv-wrapper bg-success rounded p-2 text-white text-end">
                                     <div class="review-bcv mt-5">
-                                        <img src="{{ $teacher->image }}" class="d-block img-cus imagebox bcv"
+                                        <img src="{{ $teacher->image }}" class="d-none d-lg-block img-cus imagebox bcv"
                                             alt="...">
                                             {{-- <div class="img-responsive-wrap">
                                                 <a href="yourlink" class="img-inner" style="background-image: url('{{ $teacher->image }}');"><img alt="Image Alt" title="Your Title" src=""></a>
                                             </div> --}}
                                         <div class="vector">
-                                            <img src="./cdn/anylearn/img/Vector.png" class="d-block img-fluid"
+                                            <img src="./cdn/anylearn/img/Vector.png" class="d-none d-lg-block img-fluid"
                                                 alt="...">
                                         </div>
-
                                     </div>
-
-                                    <div class="fw-bold text-start p-5 text-title"> Ban cố vấn & <br> chuyên gia</div>
-                                    <div class="content-bcv text-start ">
-                                        <p class="text-white">
-                                        <h5>{{ $teacher->name }}</h5>{{ substr($teacher->introduce,0,120) }}</p>
-                                        <a href="/ref/anylearn?role=teacher"
-                                            class="btn border rounded rounded-pill border-white btn btn-outline-success fs-6 fw-bold ps-4 pe-4 float-end mb-5 text-white">ĐĂNG
+                                    <div class="fw-bold fs-1 text-start p-5 pb-3 text-title mb-0"> Ban cố vấn & <br> chuyên gia</div>
+                                    
+                                    <div class="content-bcv text-start">
+                                        {{-- <img src="{{ $teacher->image }}" class="d-block d-lg-none img-cus2 bcv mt-0"> --}}
+                                        <p class="text-white mr-2 card-body">
+                                        <h5>{{ $teacher->name }}</h5>{{ substr($teacher->introduce,0,222) }}</p>    
+                                    </div>
+                                    <a href="/ref/anylearn?role=teacher" style="position: absolute; left:50px; bottom:15px"
+                                            class="border rounded rounded-pill border-white btn btn-outline-success fw-bold text-white">ĐĂNG
                                             KÝ LÀM CHUYÊN GIA <i class="fas fa-arrow-right"></i></a>
-                                    </div>
                                 </div>
                             </div>
                         </div>
