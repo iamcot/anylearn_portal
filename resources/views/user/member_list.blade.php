5313003ee21277b02b3e9b70c8435eb62fc3b10b
Quangnkts <quangnkts1908037@fpt.edu.vn>
2022-10-11 15:00:55 +0700

    AL-120 <fix collum>

--- resources/views/user/member_list.blade.php
+++ resources/views/user/member_list.blade.php
@@ -80,9 +80,9 @@
                     <th class="text-center">Boost</th>
                     <th width="10%" scope="col">Vai trò</th>
                     <th width="15%" scope="col">Họ tên</th>
-                    <th width="10%" scope="col">SDT</th>
-                    <th width="10%" scope="col">Email</th>
-                    <th width="15%" scope="col">Address</th>
+                    <th width="5%" scope="col">SDT</th>
+                    <th width="5%%" scope="col">Email</th>
+                    <th width="10%" scope="col">Address</th>
                     <th>Ví C</th>
                     <th width="10%" scope="col">Người G/T</th>
                     <th width="5%" scope="col">H/H</th>
