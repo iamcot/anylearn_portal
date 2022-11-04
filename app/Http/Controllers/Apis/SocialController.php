7fb83d6b6060cb884bb822cf21fc1a54338c4ac5
thang.truong <thang.truong@lazada.com>
2022-11-04 10:47:14 +0700

    fix api

--- app/Http/Controllers/Apis/SocialController.php
+++ app/Http/Controllers/Apis/SocialController.php
@@ -41,7 +41,7 @@ class SocialController extends Controller
         });
         return response()->json([
             'profile' => $user,
-            'post' => $dbPosts,
+            'posts' => $dbPosts,
         ]);
     }
 
