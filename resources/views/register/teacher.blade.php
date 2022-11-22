8dea506b532d78f40159ad5ce3088ae99b9cfd09
Quang <gmqn01@gmail.com>
2022-11-22 13:38:29 +0700

    AL-103 <update img>

--- resources/views/register/teacher.blade.php
+++ resources/views/register/teacher.blade.php
@@ -1,46 +1,86 @@
 @extends('register.form')
 @section('header1')
-@lang('Cùng anyLEARN xây dựng cộng đồng học tập không giới hạn')
+    @lang('Cùng anyLEARN xây dựng cộng đồng học tập không giới hạn')
 @endsection
 @section('header2')
-@lang('Đăng ký anyPROFESSOR')
+    @lang('Đăng ký anyPROFESSOR')
 @endsection
 @section('introduce')
-<div>
-    <img src="/cdn/img/register/form_header.png" alt="">
-</div>
-<div class="m2">
-    <img src="/cdn/img/register/teacher_step.png" alt="">
-</div>
-<div class="row p1">
-    <div class="col-3">
-        <img src="/cdn/img/register/teacher_step1.png" alt="">
-    </div>
-    <div class="col-3">
-        <img src="/cdn/img/register/teacher_step2.png" alt="">
-    </div>
-    <div class="col-3">
-        <img src="/cdn/img/register/teacher_step3.png" alt="">
-    </div>
-    <div class="col-3">
-        <img src="/cdn/img/register/teacher_step4.png" alt="">
-    </div>
-</div>
-<div class="m2">
-    <img src="/cdn/img/register/reason.png" alt="">
-</div>
-<div class="row p1">
-    <div class="col-3">
-        <img src="/cdn/img/register/teacher_r1.png" alt="">
-    </div>
-    <div class="col-3">
-        <img src="/cdn/img/register/teacher_r2.png" alt="">
-    </div>
-    <div class="col-3">
-        <img src="/cdn/img/register/teacher_r3.png" alt="">
-    </div>
-    <div class="col-3">
-        <img src="/cdn/img/register/teacher_r4.png" alt="">
-    </div>
-</div>
+    @if (Session::get('locale') == null || Session::get('locale') == 'vi')
+        <div>
+            <img src="/cdn/img/register/form_header.png" alt="">
+        </div>
+        <div class="m2">
+            <img src="/cdn/img/register/teacher_step.png" alt="">
+        </div>
+        <div class="row p1">
+            <div class="col-3">
+                <img src="/cdn/img/register/teacher_step1.png" alt="">
+            </div>
+            <div class="col-3">
+                <img src="/cdn/img/register/teacher_step2.png" alt="">
+            </div>
+            <div class="col-3">
+                <img src="/cdn/img/register/teacher_step3.png" alt="">
+            </div>
+            <div class="col-3">
+                <img src="/cdn/img/register/teacher_step4.png" alt="">
+            </div>
+        </div>
+        <div class="m2">
+            <img src="/cdn/img/register/reason.png" alt="">
+        </div>
+        <div class="row p1">
+            <div class="col-3">
+                <img src="/cdn/img/register/teacher_r1.png" alt="">
+            </div>
+            <div class="col-3">
+                <img src="/cdn/img/register/teacher_r2.png" alt="">
+            </div>
+            <div class="col-3">
+                <img src="/cdn/img/register/teacher_r3.png" alt="">
+            </div>
+            <div class="col-3">
+                <img src="/cdn/img/register/teacher_r4.png" alt="">
+            </div>
+        </div>
+    @else
+        <div>
+            <img src="/cdn/img/register/form_header_en.png" alt="">
+        </div>
+        <div class="m2">
+            <img src="/cdn/img/register/teacher_step_en.png" alt="">
+        </div>
+        <div class="row p1">
+            <div class="col-3">
+                <img src="/cdn/img/register/step1_en.png" alt="">
+            </div>
+            <div class="col-3">
+                <img src="/cdn/img/register/step2_en.png" alt="">
+            </div>
+            <div class="col-3">
+                <img src="/cdn/img/register/step3_en.png" alt="">
+            </div>
+            <div class="col-3">
+                <img src="/cdn/img/register/step4_en.png" alt="">
+            </div>
+        </div>
+        <div class="m2">
+            <img src="/cdn/img/register/reason_en.png" alt="">
+        </div>
+        <div class="row p1">
+            <div class="col-3">
+                <img src="/cdn/img/register/school_r1_en.png" alt="">
+            </div>
+            <div class="col-3">
+                <img src="/cdn/img/register/school_r2_en.png" alt="">
+            </div>
+            <div class="col-3">
+                <img src="/cdn/img/register/school_r3_en.png" alt="">
+            </div>
+            <div class="col-3">
+                <img src="/cdn/img/register/school_r4_en.png" alt="">
+            </div>
+        </div>
+    @endif
 @endsection
