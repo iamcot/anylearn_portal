19723cd0a474c95c134952da905752b89540ff48
Quang <gmqn01@gmail.com>
2022-11-29 10:00:20 +0700

    AL-144 <fix layout>

--- resources/views/anylearn/home/classes.blade.php
+++ resources/views/anylearn/home/classes.blade.php
@@ -1,30 +1,32 @@
-@inject('itemServ', 'App\Services\ItemServices')
+@inject('itemServ','App\Services\ItemServices')
 <section class="carousel4">
-    <div class="mxauto myauto justifycontentcenter">
-        {{-- {{ <div id="{{ $carouselId }}">
-            <h2 class="m2 fwbold textuppercase">{{ __($title) }}</h2>
-            <div class="owlcarousel owltheme" >
-
-                @foreach ($data as $class)
-                <a class="p1 classBox" href="{{ $itemServ>classUrl($class>id) }}">
-                        <div class="cardimg">
+    <div class="mx-auto my-auto justify-content-center">
+        <div id="{{ $carouselId }}">
+            <h2 class="m-2 fw-bold text-uppercase">{{ $title }}</h2>
+            <div class="owl-carousel owl-theme" >
+                @foreach($data as $class)
+                <a class="p-1 classBox" href="{{ $itemServ->classUrl($class->id) }}" data-spm="topclass.{{ $class->id }}">
+                    <div class="card border-0 shadow-sm">
+                        <div class="card-img">
                             <div class="imagebox">
-                                <img src="{{ $class>image }}" class="imgfluid">
+                                <img src="{{ $class->image }}" class="img-fluid">
                             </div>
-                            <div class="classtitle mt1 fwbold p1 textsuccess">@if ($class > is_hot) <span class="badge bgdanger "><i class="fas fafire"></i> HOT</span> @endif {{ $class>title }}</div>
-                            <div class="p1">
-                                <span class="textdanger fwbold">{{ number_format($class>price, 0, ',', '.') }} đ</span>
+                            <div class="class-title mt-1 fw-bold p-1 text-success">@if($class->is_hot) <span class="badge bg-danger "><i class="fas fa-fire"></i> HOT</span> @endif {{ $class->title }}</div>
+                            <div class="p-1">
+                                <span class="text-danger fw-bold">{{ number_format($class->price, 0, ',', '.') }} đ</span>
                             </div>
-                            <div class="">@include('anylearn.widget.rating', ['score' => $class>rating ?? 0])</div>
-                            <div class="classprice ps1 pe1">
-                                <span class=" textsecondary">{{ $class>short_content }}</span>
+                            <div class="">@include('anylearn.widget.rating', ['score' => $class->rating ?? 0])</div>
+                            <div class="class-price ps-1 pe-1">
+                                <span class=" text-secondary">{{ $class->short_content }}</span>
                             </div>
-                            <div class="p2 textcenter mb2">
-                                <button class="btn btnwhite roundedpill shadow border0 w75 textsuccess fwbold">@lang('CHI TIẾT')</button>
+                            <div class="p-2 text-center mb-2">
+                                <button class="btn btn-white rounded-pill shadow border-0 w-75 text-success fw-bold">@lang('CHI TIẾT')</button>
                             </div>
                         </div>
+                    </div>
+                </a>
                 @endforeach
             </div>
-        </div> }} --}}
+        </div>
     </div>
 </section>
