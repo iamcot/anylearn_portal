0e5b106d503f56fce9ffa860dc64cc11fca8a638
Quang <gmqn01@gmail.com>
2022-11-29 10:45:01 +0700

    AL-144 <fix>

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -433,30 +433,30 @@ class ConfigController extends Controller
         $this->data['configs'] = [
             [], [], [], [], [], //5
         ];
-        if ($config) {
-            $values = json_decode($config->value, true);
-            for ($i = 0; $i < count($this->data['configs']); $i++) {
-                if (!empty($values[$i])) {
-                    if (empty($values[$i]['title'])) {
-                            $values[$i]['title'] = json_encode([
-                                'vi' => null,
-                                'en' => null,
-                            ]);
-                    }
-                    if($values[$i]['title']){
-                        $values[$i]['title'] = json_encode([
-                            'vi' => $values[$i]['title'],
-                            'en' => $values[$i]['title'],
-                        ]);
-                    }
-                 $values[$i]['title'] = json_decode($values[$i]['title'], true);
-                }
-                // dd($values[$i]['title']);
-                $config = empty($values[$i]) ? [] : $values[$i];
-                    $this->data['configs'][$i] = $config;
-            }
-        }
-        //  dd($this->data['configs']);
+        // if ($config) {
+        //     $values = json_decode($config->value, true);
+        //     for ($i = 0; $i < count($this->data['configs']); $i++) {
+        //         if (!empty($values[$i])) {
+        //             if (empty($values[$i]['title'])) {
+        //                     $values[$i]['title'] = json_encode([
+        //                         'vi' => null,
+        //                         'en' => null,
+        //                     ]);
+        //             }
+        //             if($values[$i]['title']){
+        //                 $values[$i]['title'] = json_encode([
+        //                     'vi' => $values[$i]['title'],
+        //                     'en' => $values[$i]['title'],
+        //                 ]);
+        //             }
+        //          $values[$i]['title'] = json_decode($values[$i]['title'], true);
+        //         }
+        //         // dd($values[$i]['title']);
+        //         $config = empty($values[$i]) ? [] : $values[$i];
+        //             $this->data['configs'][$i] = $config;
+        //     }
+        // }
+          dd($this->data['configs']);
         $this->data['navText'] = __('Quản lý Các Khoá học Đặc biệt trên HOME APP');
         return view('config.homeclasses', $this->data);
     }
