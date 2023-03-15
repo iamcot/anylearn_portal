43d11e1e1574453c1d29ec6a53fe6910a2feee71
Quang <gmqn01@gmail.com>
2023-03-15 15:17:51 +0700

    BO-211

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,6 +31,7 @@ use App\Services\InteractServices;
 use App\Services\TransactionService;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Redirect;
+use PSpell\Config;
 use Symfony\Component\VarDumper\Cloner\Data;
 use Vanthao03596\HCVN\Models\District;
 use Vanthao03596\HCVN\Models\Province;
@@ -307,7 +308,6 @@ class UserController extends Controller
         $this->data['courses'] = $courses;
         $this->data['userC'] = $userC;
         return view(env('TEMPLATE', '') . 'me.childhistory', $this->data);
-
     }
     public function mePassword(Request $request)
     {
@@ -641,7 +641,7 @@ class UserController extends Controller
         $user = Auth::user();
         $orderDetailM = new OrderDetail();
         $data = $orderDetailM->usersOrders($user->id);
-        $this -> data['data'] = $data;
+        $this->data['data'] = $data;
         return view(env('TEMPLATE', '') . 'me.user_orders', $this->data);
     }
 
@@ -724,10 +724,34 @@ class UserController extends Controller
     }
     public function withdraw(Request $request)
     {
-
         $user = Auth::user();
-        $contract = Contract::where('user_id',$user->id)->where('status',99)->first();
-        // dd($contract->bank_name);
+        if ($request->input('withdraw')) {
+            $input = $request->all();
+            if (Hash::check($input['password'], $user->password)) {
+                Transaction::create([
+                    'user_id' => $user->id,
+                    'type' => ConfigConstants::TRANSACTION_WITHDRAW,
+                    'amount' => $input['withdraw'],
+                    'ref_amount' => $input['withdraw'],
+                    'pay_method' => UserConstants::WALLET_M,
+                    'pay_info' => '',
+                    'content' => 'Rút ' . $input['withdraw'] . ' cho đơn đối tác #' . ($user->id) . ' ' . $user->name,
+                    'status' => ConfigConstants::TRANSACTION_STATUS_PENDING,
+                    'order_id' => null
+                ]);
+                return redirect()->back()->with('notify', 'Lệnh rút Tiền đã được gởi đi');
+            } else {
+                return redirect()->back()->with('notify', 'Mật khẩu không chính xác');
+            }
+        }
+        $history = DB::table('transactions')->where('user_id', $user->id)->where('pay_method','wallet_m')->where('type','!=',ConfigConstants::TRANSACTION_DEPOSIT)->orderByDesc('created_at')->get();
+        $totalAmount = DB::table('transactions')
+            ->where('type', 'withdraw')
+            ->where('status', 0)
+            ->sum('amount');
+        $contract = Contract::where('user_id', $user->id)->where('status', 99)->first();
+        $this->data['history'] = $history;
+        $this->data['totalAmount'] = $totalAmount;
         $this->data['user'] = $user;
         $this->data['contract'] = $contract;
         return view(env('TEMPLATE', '') . 'me.withdraw', $this->data);
--- resources/views/anylearn/me/withdraw.blade.php
+++ resources/views/anylearn/me/withdraw.blade.php
@@ -33,37 +33,31 @@
             <div class="card shadow mt-3">
                 <div class="card-header"><strong>Lịch Sử Giao Dịch</strong></div>
                 <div class="card-body">
-                    {{-- <table class="table">
+                    <table class="table">
                         <thead>
                             <tr>
                                 <th>Ngày</th>
-                                <th>Nội Dung</th>
+                                <th width=40%>Nội Dung</th>
                                 <th>Số Tiền</th>
+                                <th>Trạng Thái</th>
                             </tr>
                         </thead>
                         <tbody>
+                            @foreach ($history as $row)
                             <tr>
-                                <td>Jan 1, 2022</td>
-                                <td>Payment received from John Smith</td>
-                                <td>$500</td>
+                                <td>{{ $row->created_at}}</td>
+                                <td>{{ $row->content }}</td>
+                                <td>{{ number_format($row->amount) }}</td>
+                                @if ($row->status == 1)
+                                <td><span class="badge badge-success">Đã duyệt</span></td>
+                                @else
+                                <td><span class="badge badge-warning">Đợt duyệt</span></td>
+                                @endif
+
                             </tr>
-                            <tr>
-                                <td>Jan 5, 2022</td>
-                                <td>Transfer to Savings Account</td>
-                                <td>-$200</td>
-                            </tr>
-                            <tr>
-                                <td>Jan 10, 2022</td>
-                                <td>Online Purchase</td>
-                                <td>-$100</td>
-                            </tr>
-                            <tr>
-                                <td>Jan 15, 2022</td>
-                                <td>Deposit from Employer</td>
-                                <td>$1000</td>
-                            </tr>
+                            @endforeach
                         </tbody>
-                    </table> --}}
+                    </table>
 
                 </div>
             </div>
@@ -78,7 +72,7 @@
                             <div class="form-group">
                                 <label for="inputCurrentBalance">Số tiền hiện có:</label>
                                 <input type="text" class="form-control"
-                                    value="{{ $user->wallet_m}}" readonly>
+                                    value="{{ number_format($user->wallet_m  - $totalAmount)}}" readonly>
                             </div>
                             <div class="form-group">
                                 <label for="inputCurrentBalance">Ngân Hàng:</label>
@@ -102,14 +96,14 @@
                             </div>
                             <div class="form-group">
                                 <label for="inputAmount">Số tiền muốn rút:</label>
-                                <input type="number" class="form-control" id="inputAmount"
+                                <input type="number" class="form-control" name="withdraw" id="inputAmount"
                                     placeholder="Nhập số tiền muốn rút" required>
                             </div>
                             <div class="form-group">
                                 <label for="inputPassword">Mật khẩu:</label>
-                                <input type="password" class="form-control" id="inputPassword" placeholder="Nhập mật khẩu">
+                                <input type="password" name="password" class="form-control" id="inputPassword" placeholder="Nhập mật khẩu">
                             </div>
-                            <button type="submit" class="btn btn-primary float-right">Xác nhận rút tiền</button>
+                            <button type="submit" class="btn btn-primary float-right" name="action" value="withdraw">Xác nhận rút tiền</button>
                         </form>
                     </div>
                 </div>
@@ -120,6 +114,6 @@
 @section('jscript')
 @parent
 <script>
-    $('#inputAmount').attr('max', {{ $user->wallet_m}});
+    $('#inputAmount').attr('max', {{ $user->wallet_m - $totalAmount}});
 </script>
 @endsection
