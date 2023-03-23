f804dcb833173e7fda4a23a2b8d775e6bf01f7cb
Quang <gmqn01@gmail.com>
2023-03-23 16:16:22 +0700

    BO-203 <update>

--- resources/views/anylearn/footer2.blade.php
+++ resources/views/anylearn/footer2.blade.php
@@ -3,29 +3,33 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-3">
-                <h4>CHĂM SÓC KHÁCH HÀNG</h4>
+                <h4>TRUNG TÂM HỖ TRỢ</h4>
                 <ul>
-                    <a href="/helpcenter"><li>Trung Tâm Trợ Giúp </li></a>
-                    <a href="/article/140/giai-phap-thanh-toan-hoc-phi-linh-hoat-va-tien-loi-cho-ba-me.html"><li>Tin Tức</li></a>
-                    <a href="/me"><li>Trung tâm tài khoản</li></a>
-                    <a href="/helpcenter"><li>Hướng Dẫn Mua Hàng</li></a>
+                    <a href="/helpcenter">
+                        <li>Trung Tâm Trợ Giúp </li>
+                    </a>
+                    <a href="/helpcenter/5/anypoint-la-gi.html">
+                        <li>anyPOINT là gì?</li>
+                    </a>
                 </ul>
             </div>
             <div class="col-sm-3">
-                <h4>CHÍNH SÁCH THANH TOÁN</h4>
+                <h4>ĐIỀU KHOẢN & CHÍNH SÁCH</h4>
                 <ul>
-                    <a href="/helpcenter/5/anypoint-la-gi.html"><li>anyPOINT</li></a>
-                    <a href="#"><li>Trả Hàng &amp; Hoàn Tiền</li></a>
-                    <a href="tel:0374900344"><li>Chăm Sóc Khách Hàng</li></a>
+                    <a href="/guide?p=guide_toc">
+                        <li>Điều Khoản Sử Dụng</li>
+                    </a>
+                    <a href="/privacy">
+                        <li>Chính Sách Bảo Mật</li>
+                    </a>
                 </ul>
             </div>
             <div class="col-sm-3">
-                <h4>VỀ ANYLEARN</h4>
-                <ul>
-                    <a target="_blank" href="https://anylearn.vn/info"><li>Giới Thiệu Về ANYLEARN</li></a>
-                    <a href="#"><li>Tuyển Dụng</li></a>
-                    <a href="/guide?p=guide_toc"><li>Điều Khoản</li></a>
-                    <a href="/privacy"><li>Chính Sách Bảo Mật</li></a>
+                <h4>TIN TỨC</h4>
+                <ul class="list-unstyled text-secondary">
+                    @foreach($itemServ->footerNews() as $news)
+                    <li><p><a data-spm="footer-news.{{ $news->id }}" href="{{ $itemServ->articleUrl($news->id) }}" class="text-secondary text-decoration-none">{{ $news->title }}</a></p></li>
+                    @endforeach
                 </ul>
             </div>
             <div class="col-sm-3">
@@ -33,25 +37,27 @@
                 <img src="/cdn/img/Visa.png" width="20%" class="img-fluid">
                 <img src="/cdn/img/mastercard.png" width="20%" class="img-fluid">
                 <img src="/cdn/img/onepay.png" width="20%" class="img-fluid">
-                {{-- <img src="/cdn/img/thong-bao-website-voi-bo-cong-thuong_grande.png" width="60%" class="img-fluid"> --}}
-                {{-- <img src="/cdn/anylearn/img/logo-color.svg" width="80%" class="img-fluid"> --}}
             </div>
-
-            <div class="col-sm-3">
+            <div class="col-sm-3 mt-2">
                 <h4>THEO DÕI CHÚNG TÔI</h4>
                 <div class="social-links">
-                    <a target="_blank" href="https://www.youtube.com/channel/UCam71id1lM8tZuMfjy2DDRw"><img src="/cdn/img/youtube.png" alt=""></a>
-                    <a target="_blank" href="https://www.facebook.com/anylearnhockhonggioihan"><img src="/cdn/img/facebook.png" alt=""></a>
-                    <a target="_blank" href="https://zalo.me/0374900344"><img src="/cdn/img/zalo.png" alt=""></a>
+                    <a target="_blank" href="https://www.youtube.com/channel/UCam71id1lM8tZuMfjy2DDRw"><img
+                            src="/cdn/img/youtube.png" alt=""></a>
+                    <a target="_blank" href="https://www.facebook.com/anylearnhockhonggioihan"><img
+                            src="/cdn/img/facebook.png" alt=""></a>
+                    <a target="_blank" href="https://zalo.me/0374900344"><img src="/cdn/img/zalo.png"
+                            alt=""></a>
                 </div>
             </div>
-            <div class="col-sm-3">
+            <div class="col-sm-3 mt-2">
                 <h4>
-                    <p>TẢI ỨNG DỤNG ANYLEARN</p>
+                    <p>TẢI ỨNG DỤNG anyLEARN</p>
                 </h4>
                 <div class="app-links">
-                    <a target="_blank" href="https://play.google.com/store/apps/details?id=vn.anylearn&hl=vi&gl=US"><img src="/cdn/img/ggplay.png" class="img-fluid" width="40%"></a>
-                    <a target="_blank" href="https://apps.apple.com/vn/app/anylearn/id1518155412"><img src="/cdn/img/appstore.png" class="img-fluid" width="45%"></a>
+                    <a target="_blank" href="https://play.google.com/store/apps/details?id=vn.anylearn&hl=vi&gl=US"><img
+                            src="/cdn/img/ggplay.png" class="img-fluid" width="40%"></a>
+                    <a target="_blank" href="https://apps.apple.com/vn/app/anylearn/id1518155412"><img
+                            src="/cdn/img/appstore.png" class="img-fluid" width="45%"></a>
                 </div>
             </div>
 
