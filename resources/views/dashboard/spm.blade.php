79a9da95d37386403dcb2f8244da43c582f69265
Xuyen <ngoxuyen214@gmail.com>
2023-03-28 18:28:11 +0700

    =Update

--- resources/views/dashboard/spm.blade.php
+++ resources/views/dashboard/spm.blade.php
@@ -1,5 +1,37 @@
 @extends('layout')
 @section('body')
+<div class="card shadow mb-3">
+    <div class="card-body row">
+        <div class="col-xs-6 col-lg-2">
+            <div class="form-group">
+                <label for="">ID</label>
+                <input value="" type="text" class="form-control" name="user_id" placeholder="ID người dùng">
+            </div> 
+        </div>
+        <div class="col-xs-6 col-lg-2">
+            <div class="form-group">
+                <label for="">Loại</label>
+                <input value="" type="text" class="form-control" name="user_type" placeholder="Loại người dùng">
+            </div>
+        </div>
+        <div class="col-xs-6 col-lg-2">
+            <div class="form-group">
+                <label for="">Từ ngày</label>
+                <input value="" type="date" class="form-control" name="date_from">
+            </div>
+        </div>
+        <div class="col-xs-6 col-lg-2">
+            <div class="form-group"> 
+                <label for="">Đến ngày</label>
+                <input value="" type="date" class="form-control" name="date_to">
+            </div>
+        </div>
+        <div class="col-xs-6 col-lg-2 d-flex flex-column justify-content-end mb-3">   
+            <button class="btn btn-primary align-items-center" name="action" value="search">Filter</button>
+        </div>
+    </div>
+</div>
+
 <div class="card shadow">
     <div class="card-body p-0">
         <table class="table table-bordered table-responsive table-striped">
@@ -26,4 +58,5 @@
     </div>
     <div class="card-footer">{{ $spms->links() }}</div>
 </div>
+
 @endsection
