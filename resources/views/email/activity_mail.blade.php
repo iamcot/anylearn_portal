edbb2818bdb76da3e04fba79a641d0ef2aee1112
Quang <gmqn01@gmail.com>
2023-03-31 15:46:14 +0700

    commit local

--- resources/views/email/activity_mail.blade.php
+++ resources/views/email/activity_mail.blade.php
@@ -8,3 +8,28 @@
 <p>Chúng tôi nhận được đăng ký Học Thử/Thăm Quan/Kiểm Tra mới từ khách hàng {{ $name }}. Xin vui lòng xem thông tin chi tiết ở trên.</p>
 <p>Trân trọng,</p>
 <p>Đội ngũ anyLEARN</p>
+
+<p>Kính gửi quý đối tác {tên}</p>,
+
+<p>anyLEARN thông báo Khóa học {tên} của đối tác vừa được đăng ký Học Thử/Thăm Quan/Kiểm Tra bởi {tên người đăng ký} vào lúc {thời gian thực lúc đăng ký}</p>
+
+<p>Lịch học:</p>
+
+<p>Địa điểm học:</p>
+
+<p>Thời gian khai giảng:</p>
+
+<p>Học phí: {số tiền}</p>
+
+<p>Giảm trừ: {số tiền}</p>
+
+Theo chương trình ưu đãi …
+Số tiền đã nhận từ KH: {số tiền thanh toán}
+Mức chiết khấu cho anyLEARN: {xx%} tương đương số tiền: {tiền}
+Số tiền thành toán cho đối tác: {tiền}
+Ngày thanh toán: trong vòng 30 ngày kể từ ngày Học viên nhập học tại cơ sở của đối tác.
+
+Quý đối tác vui lòng xác nhận và phản hồi email này nếu có thay đổi thông tin học tập của khóa học.
+
+Trân trọng cảm ơn!
+
