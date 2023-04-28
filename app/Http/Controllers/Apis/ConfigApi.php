cc5beefaca1c5c54d6d5d6b3a5876fab50cf262e
Tina <ngoxuyen214@gmail.com>
2023-04-28 14:36:50 +0700

    Testing

--- app/Http/Controllers/Apis/ConfigApi.php
+++ app/Http/Controllers/Apis/ConfigApi.php
@@ -79,9 +79,9 @@ class ConfigApi extends Controller
             }
         }
         
-        /*$data['config'] = $homeConfig;
+        $data['config'] = $homeConfig;
         $data['classes'] = $homeClasses; 
-        $data['banners'] = $bannerConfig;*/
+        $data['banners'] = $bannerConfig;
 
         $homeS = new HomeServices(); 
         $data['asks'] = $homeS->getAsk();
@@ -91,13 +91,13 @@ class ConfigApi extends Controller
         $data['recommendations'] = $homeS->setTemplates('/', 'anyLEARN đề Xuất', $homeS->getRecommendations());        
         $data['pointBox'] = $data['j4u'] = $data['repurchaseds'] = []; 
     
-        /*if ($role) { 
+        if ($role) { 
             $user  = $request->get('_user');   
 
             $data['j4u'] = $homeS->setTemplates('/', 'Có thể bạn sẽ thích', $homeS->getJ4u($user));
             $data['repurchaseds'] = $homeS->setTemplates('/', 'Đăng ký lại', $homeS->getRepurchaseds($user));
             $data['pointBox'] = $homeS->getPointBox($user);
-        }*/
+        }
 
         return response()->json($data);
     } 
