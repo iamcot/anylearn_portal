efe2a1903dfb52382eb07e0854195d94cea7360b
Tina <ngoxuyen214@gmail.com>
2023-05-10 17:13:48 +0700

    Testing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -159,8 +159,6 @@ class UserController extends Controller
                             $countUpdate += User::where('phone', $row[4])->update($data);
                         } else {
                             // Log::debug($row);
-
-                            dd($row);
                             User::create([
                                 'name' => $row[0],
                                 'dob' => $row[1],
