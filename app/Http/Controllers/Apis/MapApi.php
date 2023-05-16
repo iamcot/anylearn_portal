1644ea9a3c976f351f2f0c803a126f2110a05935
Tina <ngoxuyen214@gmail.com>
2023-05-16 17:44:59 +0700

    Testing

--- app/Http/Controllers/Apis/MapApi.php
+++ app/Http/Controllers/Apis/MapApi.php
@@ -4,8 +4,27 @@ namespace App\Http\Controllers\Apis;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class MapApi extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $data = [];
+        if ($request->all()) {
+            $top = explode(',', $request->get('top'));
+            $bot = explode(',', $request->get('bot'));
+            
+            $data = DB::table('users')
+                ->join('user_locations as ul', 'ul.user_id', '=', 'users.id')
+                ->where('users.role', 'school')
+                ->whereBetween('ul.longitude', [$top[0], $bot[0]])
+                ->whereBetween('ul.latitude', [$top[1], $bot[1]])
+                ->get();
+                //status
+
+        }
+        return response()->json($data);
+    }
 }
+
