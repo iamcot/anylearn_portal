dea9a9bad2bd15bb4f3e5f1ba985c28715aaa6c1
Tina <ngoxuyen214@gmail.com>
2023-05-16 15:44:29 +0700

    Testing

--- app/Http/Controllers/Apis/SearchFilterApi.php
+++ app/Http/Controllers/Apis/SearchFilterApi.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Spm;
+use Illuminate\Support\Facades\DB;
 use Vanthao03596\HCVN\Models\Province;
 
 class SearchFilterApi extends Controller
@@ -13,20 +14,22 @@ class SearchFilterApi extends Controller
     public function index(Request $request, $role = 'guest')
     {
         $data['searcheds'] = Spm::where('spmc', 'search')
-            ->distinct('extra')
+            ->select(DB::raw('extra, max(created_at) as created_at'))
+            ->groupBy('extra')
             ->orderByDesc('created_at')
             ->pluck('extra');
 
-        $data['provinces'] = Province::orderBy('name')->get();
-        $data['categories'] = Category::orderBy('title')->get();
+        $data['provinces'] = Province::select('code', 'name')->orderBy('name')->get();
+        $data['categories'] = Category::select('id', 'title')->orderBy('title')->get();
 
         if($role == 'member') {
             $user = $request->get('_user');
             $data['searcheds'] = Spm::where('spmc', 'search')
-                ->where('user_id', $user->id)
-                ->distinct('extra')
-                ->orderByDesc('created_at')
-                ->pluck('extra');
+            ->where('user_id', $user->id)
+            ->select(DB::raw('extra, max(created_at) as created_at'))
+            ->groupBy('extra')
+            ->orderByDesc('created_at')
+            ->pluck('extra');
         }
 
         return response()->json($data);
