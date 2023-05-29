2b2387224f23de9ddafac7a32944a19f6dd56f3d
Tina <ngoxuyen214@gmail.com>
2023-05-29 14:50:38 +0700

    Testing

--- resources/views/anylearn3/index.blade.php
+++ resources/views/anylearn3/index.blade.php
@@ -2,18 +2,7 @@
 <html lang="en">
     <head>
     <head>
-        <meta charset="utf-8"/>
-        <link rel="icon" href="/cdn/anylearn3/0.0.1/logo.png"/>
-        <meta name="viewport" content="width=device-width,initial-scale=1"/>
-        <meta name="theme-color" content="#000000"/>
-        <meta name="description" content="Web site created using create-react-app"/>
-        <link rel="apple-touch-icon" href="/cdn/anylearn3/0.0.1/logo.png"/>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"/>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"/>
-        <link rel="manifest" href="/cdn/anylearn3/0.0.1/manifest.json"/>
-        <title>anyLEARN</title>
-        <script defer="defer" src="/cdn/anylearn3/0.0.1/static/js/main.033b3376.js"></script>
-        <link href="/cdn/anylearn3/0.0.1/static/css/main.c74cd313.css" rel="stylesheet">
+    <meta charset="utf-8"/><link rel="icon" href="/cdn/anylearn3/0.0.1/logo.png"/><meta name="viewport" content="width=device-width,initial-scale=1"/><meta name="theme-color" content="#000000"/><meta name="description" content="Web site created using create-react-app"/><link rel="apple-touch-icon" href="/cdn/anylearn3/0.0.1/logo.png"/><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"/><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"/><link rel="manifest" href="/cdn/anylearn3/0.0.1/manifest.json"/><title>anyLEARN</title><script defer="defer" src="/cdn/anylearn3/0.0.1/static/js/main.2b2da7ef.js"></script><link href="/cdn/anylearn3/0.0.1/static/css/main.c74cd313.css" rel="stylesheet">
         
         <!-- Laravel Styles -->
         <link rel="canonical" href="@yield('canonical')" />
