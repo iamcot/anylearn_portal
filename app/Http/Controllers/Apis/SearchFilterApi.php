fb0b606b12962c2299b119bf6a5e9d44b92ec3e2
Tina <ngoxuyen214@gmail.com>
2023-05-30 16:13:05 +0700

    Testing

--- app/Http/Controllers/Apis/SearchFilterApi.php
+++ app/Http/Controllers/Apis/SearchFilterApi.php
@@ -36,7 +36,7 @@ class SearchFilterApi extends Controller
             $data['lastSearch'] = Spm::where('spmc', 'search')
                 ->whereNotNull('extra')
                 ->where('user_id', $request->get('_user')->id)
-                ->select(DB::raw('extra, max(created_at) as created_at'))
+                ->select(DB::raw('id, extra, max(created_at) as created_at'))
                 ->groupBy('extra')
                 ->orderByDesc('id')
                 ->take(ConfigConstants::CONFIG_NUM_LAST_SEARCH_DISPLAY)
