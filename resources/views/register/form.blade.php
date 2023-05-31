03ee5bbc0a4662875e9aabd32ed62b820608e715
Tina <ngoxuyen214@gmail.com>
2023-05-31 12:00:08 +0700

    Testing

--- resources/views/register/form.blade.php
+++ resources/views/register/form.blade.php
@@ -25,7 +25,8 @@
                             value="{{ !empty($user) ? $user->refcode : old('ref') }}">
                         <input type="hidden" name="role" value="{{ $role }}">
                         <input id="sale_id" type="hidden" name="sale_id" value="{{ request('s') }}">
-                        <div>
+                        <div >
+                        <div class="my-1">
                             <h6>{{__('Thông tin tài khoản:')}}</h6>
                             <div class="form-group row">
                                 <label for="name" class="col-md-4 col-form-label text-md-end"
@@ -95,66 +96,68 @@
                             </div>
                         </div>
                         @if (request('r') !== 'member')
-                            <h2>{{ __('Thông tin doanh nghiệp:')}}</h2>
-                            <div class="form-group row">
-                                <label for="business_certificate"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Số giấy chứng nhận ĐKDN') }}
-                                    *</label>
-                                <div class="col-md-8">
-                                    <input id="business_certificate" type="text"
-                                        class="form-control @error('business_certificate') is-invalid @enderror"
-                                        name="business_certificate" value="{{ old('business_certificate') }}"
-                                        autocomplete="off" autofocus required >
-                                    @error('business_certificate')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                            <div>
+                                <h6>{{__('Thông tin doanh nghiệp:')}}</h6>
+                                <div class="form-group row">
+                                    <label for="business_certificate"
+                                        class="col-md-4 col-form-label text-md-end">{{ __('Số giấy chứng nhận ĐKDN') }}
+                                        *</label>
+                                    <div class="col-md-8">
+                                        <input id="business_certificate" type="text"
+                                            class="form-control @error('business_certificate') is-invalid @enderror"
+                                            name="business_certificate" value="{{ old('business_certificate') }}"
+                                            autocomplete="off" autofocus required >
+                                        @error('business_certificate')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="first_issued_date"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Ngày cấp lần đầu') }}*</label>
-                                <div class="col-md-8">
-                                    <input id="first_issued_date" type="date"
-                                        class="form-control datepicker @error('first_issued_date') is-invalid @enderror"
-                                        name="first_issued_date" value="{{ old('first_issued_date') }}" autocomplete="off"
-                                        autofocus required >
-                                    @error('first_issued_date')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label for="first_issued_date"
+                                        class="col-md-4 col-form-label text-md-end">{{ __('Ngày cấp lần đầu') }}*</label>
+                                    <div class="col-md-8">
+                                        <input id="first_issued_date" type="date"
+                                            class="form-control datepicker @error('first_issued_date') is-invalid @enderror"
+                                            name="first_issued_date" value="{{ old('first_issued_date') }}" autocomplete="off"
+                                            autofocus required >
+                                        @error('first_issued_date')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div class="form-group row">
-                                <label for="issued_by"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Cấp bởi') }}*</label>
-                                <div class="col-md-8">
-                                    <input id="issued_by" type="text"
-                                        class="form-control @error('issued_by') is-invalid @enderror" name="issued_by"
-                                        value="{{ old('issued_by') }}" autocomplete="off" autofocus required >
-                                    @error('issued_by')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label for="issued_by"
+                                        class="col-md-4 col-form-label text-md-end">{{ __('Cấp bởi') }}*</label>
+                                    <div class="col-md-8">
+                                        <input id="issued_by" type="text"
+                                            class="form-control @error('issued_by') is-invalid @enderror" name="issued_by"
+                                            value="{{ old('issued_by') }}" autocomplete="off" autofocus required >
+                                        @error('issued_by')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="headquarters_address"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Địa chỉ trụ sở') }}*</label>
-                                <div class="col-md-8">
-                                    <input id="headquarters_address" type="text"
-                                        class="form-control @error('headquarters_address') is-invalid @enderror"
-                                        name="headquarters_address" value="{{ old('headquarters_address') }}"
-                                        autocomplete="off" autofocus required>
-                                    @error('headquarters_address')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label for="headquarters_address"
+                                        class="col-md-4 col-form-label text-md-end">{{ __('Địa chỉ trụ sở') }}*</label>
+                                    <div class="col-md-8">
+                                        <input id="headquarters_address" type="text"
+                                            class="form-control @error('headquarters_address') is-invalid @enderror"
+                                            name="headquarters_address" value="{{ old('headquarters_address') }}"
+                                            autocomplete="off" autofocus required>
+                                        @error('headquarters_address')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
                             </div>
                         @endif
