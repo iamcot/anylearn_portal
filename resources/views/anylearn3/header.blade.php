b77eb1c489e9ce8bc78905efb2615a1daff46298
Tina <ngoxuyen214@gmail.com>
2023-05-31 17:18:03 +0700

    Testing

--- resources/views/anylearn3/header.blade.php
+++ resources/views/anylearn3/header.blade.php
@@ -1,7 +1,21 @@
 <header>
-    <div>
-        <ul>
-            _('Kênh học tập')
-        </ul>
+    <div class="bg-blue-600">
+        <div class="mx-auto max-w-5xl py-1 flex justify-between">
+            <div class="flex items-center">
+                <a class="text-white hover:text-gray-300 rounded-md px-2 py-1 text-xs font-medium hidden  lg:block">Kênh học tập</a>
+                <a href="#" class="text-white hover:text-gray-300 px-1 py-1 text-xs font-medium hidden  lg:block">|</a>
+                <a class="text-white hover:text-gray-300 rounded-md px-2 py-1 text-xs font-medium hidden  lg:block">Trở thành đối tác anyLEARN</a>
+                <a href="#" class="text-white hover:text-gray-300 px-1 py-1 text-xs font-medium hidden  lg:block">|</a>
+                <a class="text-white hover:text-gray-300 rounded-md px-2 py-1 text-xs font-medium hidden  lg:block">Tải ứng dụng</a>
+            </div>
+                <div class="flex items-center">
+                    <a href="#" class="text-white hover:text-gray-300 rounded-md px-2 py-1 text-xs font-medium hidden  lg:block"><FontAwesomeIcon icon={faBell} /> Thông báo</a>
+                    <a href="#" class="text-white hover:text-gray-300 px-1 py-1 text-xs font-medium hidden  lg:block">|</a>
+                    <a class="text-white hover:text-gray-300 rounded-md px-2 py-1 text-xs font-medium hidden  lg:block"><FontAwesomeIcon icon={faQuestionCircle} /> Hỗ trợ</a>
+                    <a class="bg-blue-700 text-white px-2 py-1 rounded-md mr-2 text-xs font-medium hidden  lg:block">Đăng nhập</a>
+                    <a class='bg-blue-700 text-white px-2 py-1 rounded-md mr-2 text-xs font-medium hidden  lg:block'>Đăng xuất</a>
+                </div>
+            </div>
+        </div>
     </div>
 </header>
