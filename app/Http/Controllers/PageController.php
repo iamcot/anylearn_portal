e1339f2af106f85556365f635f3b48863fba8ad6
Tina <ngoxuyen214@gmail.com>
2023-05-31 11:27:10 +0700

    Testing

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -130,6 +130,7 @@ class PageController extends Controller
     }
     public function ref(Request $request, $code = "")
     {
+        dd(1);
         if (empty($code)) {
             return redirect('/');
         }
