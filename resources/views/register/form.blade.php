d599a0e5c39bb9c8f1e7189712a9d7b309db578f
Tina <ngoxuyen214@gmail.com>
2023-05-31 12:56:33 +0700

    Testing

--- resources/views/register/form.blade.php
+++ resources/views/register/form.blade.php
@@ -92,11 +92,10 @@
                                 </div>
                             </div>
                         @else 
-                            <div class="my-4">
-                                <h6 class="mb-4">{{ __('Thông tin tài khoản:') }}</h6>
+                            <div class="my-2 ">
+                                <h6 class="mb-2">{{ __('Thông tin tài khoản:') }}</h6>
                                 <div class="form-group row">
-                                    <label for="name" class="col-md-4 col-form-label text-md-end"
-                                        id="label_name">{{ __('Tên doanh nghiệp') }} *</label>
+                                    <label for="name" class="col-md-4 col-form-label text-md-end" id="label_name">{{ __('Tên doanh nghiệp') }} *</label>
                                     <div class="col-md-8">
                                         <input id="name" type="text"
                                             class="form-control @error('name') is-invalid @enderror" name="name"
@@ -109,8 +108,7 @@
                                     </div>
                                 </div>
                                 <div class="form-group row">
-                                    <label for="phone" class="col-md-4 col-form-label text-md-end">{{ __('Số điện thoại') }}
-                                        *</label>
+                                    <label for="phone" class="col-md-4 col-form-label text-md-end">{{ __('Số điện thoại') }} *</label>
                                     <div class="col-md-8">
                                         <input id="phone" type="text"
                                             class="form-control @error('phone') is-invalid @enderror" name="phone"
@@ -123,7 +121,7 @@
                                     </div>
                                 </div>
                                 <div class="form-group row">
-                                    <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }}*</label>
+                                    <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }} *</label>
                                     <div class="col-md-8">
                                         <input id="email" type="text"
                                             class="form-control @error('email') is-invalid @enderror" name="email"
@@ -136,9 +134,7 @@
                                     </div>
                                 </div>
                                 <div class="form-group row">
-                                    <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Mật khẩu') }}
-                                        *</label>
-
+                                    <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Mật khẩu') }} *</label>
                                     <div class="col-md-8">
                                         <input id="password" type="password"
                                             class="form-control @error('password') is-invalid @enderror" name="password" required
@@ -159,9 +155,8 @@
                                     </div>
                                 </div>
                             </div>
-
                             <div>
-                                <h6 class="mb-4">{{ __('Thông tin doanh nghiệp:') }}</h6>
+                                <h6 class="mb-2">{{ __('Thông tin doanh nghiệp:') }}</h6>
                                 <div class="form-group row">
                                     <label for="business_certificate"
                                         class="col-md-4 col-form-label text-md-end">{{ __('Số giấy chứng nhận ĐKDN') }}
