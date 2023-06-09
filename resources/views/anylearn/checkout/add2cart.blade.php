96c63b739c0b9a458bad2f4bbc29d76415a9917a
thang.truong <thang.truong@lazada.com>
2023-06-09 18:10:33 +0700

    fix

--- resources/views/anylearn/checkout/add2cart.blade.php
+++ resources/views/anylearn/checkout/add2cart.blade.php
@@ -106,8 +106,11 @@
                                 @endforeach
                             </div>
                         @else
-                            <p class="p-3">Lịch học bắt đầu từ ngày {{ date('d/m/Y', strtotime($item->date_start)) }} </p>
+                            <p class="p-3">Lịch học bắt đầu từ ngày {{ date('d/m/Y', strtotime($item->date_start)) }} 
                             @if($item->time_start)<br>Giờ học {{ $item->time_start }}.@endif
+
+                            </p>
+                         
                         @endif
                     </div>
                 </div>
