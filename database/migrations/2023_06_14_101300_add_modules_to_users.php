2e4c7bb1cf945b6ca9cd8c34fabf2eea95286e9f
thang.truong <thang.truong@lazada.com>
2023-06-16 21:51:32 +0700

    fix

--- database/migrations/2023_06_14_101300_add_modules_to_users.php
+++ database/migrations/2023_06_14_101300_add_modules_to_users.php
@@ -14,7 +14,7 @@ class AddModulesToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('modules')->nullable();
+            $table->text('modules')->nullable();
         });
     }
 
