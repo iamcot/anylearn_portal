6d9189d32afb403ba84b6b5e2d5d7076e4b14f7e
Tina <ngoxuyen214@gmail.com>
2023-06-22 17:32:54 +0700

    Update voucher

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -499,14 +499,16 @@ class TransactionService
         $order = Order::find($orderId);
         $value = floatval($value);
         $amount = false;
+        $ruleMax = $ruleMax < 1 && $ruleMax > 0 ? $value / $ruleMax : $ruleMax;
+       
         if ($order->amount >= $ruleMin && $order->amount < $ruleMax) {
             $amount = $order->amount - ($value * $order->amount / $ruleMax);
-        } 
-        else if ($value >= 1000) {
+        } else if ($value >= 1000) {
             $amount =  ($value > $order->amount) ? 0 : ($order->amount - $value);
         } else if ($value > 0 && $value < 1) {
             $amount = $order->amount - ($order->amount * $value);
         }  
+
         if ($amount === false) {
             return false;
         }
