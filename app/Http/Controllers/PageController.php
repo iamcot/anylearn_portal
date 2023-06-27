c76e360c47d1166731436c3e6441cbb27871164e
Tina <ngoxuyen214@gmail.com>
2023-06-27 10:49:52 +0700

    Testing

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -139,7 +139,7 @@ class PageController extends Controller
             return redirect('/');
         }
         // Setup the previous page url
-        $request->session->refresh();
+        $request->session()->reflash();
         $urlPrevious = url()->previous();
         if (explode('?', $urlPrevious)[0] != url()->current() && $urlPrevious != route('login')) {
             session(['cb' => $urlPrevious]);            
