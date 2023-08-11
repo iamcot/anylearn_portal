19924e96d03da1629954d0e606292ccdc4ca071b
Quang <gmqn01@gmail.com>
2023-08-11 15:29:14 +0700

    update v3

--- resources/views/anylearn3/header.blade.php
+++ resources/views/anylearn3/header.blade.php
@@ -25,7 +25,7 @@
                 @else
                     <a href="{{ route('me.dashboard') }}" class="text-white py-1 rounded-md">Trang quản lý</a>
                     <span class="text-white hover:text-gray-300 px-1 py-1">|</span>
-                    <a href="{{ route('auth.logout') }}" class="text-white pl-2 py-1 rounded-md">Đăng xuất</a>
+                    <a href="{{ route('auth.logout') }}" class="text-white py-1 rounded-md">Đăng xuất</a>
                 @endif
             </div>
         </div>
