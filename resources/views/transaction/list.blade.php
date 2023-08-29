e16a12094d82bbf86a932261424999533e7610e0
Tina <ngoxuyen214@gmail.com>
2023-08-29 18:04:47 +0700

    Test

--- resources/views/transaction/list.blade.php
+++ resources/views/transaction/list.blade.php
@@ -43,7 +43,7 @@
                     <td class="text-center" scope="row">{{ number_format($row->amount) }}</td>
                     <td class="text-center" scope="row">
                         @if($row->pay_info) 
-                            {{ dd(row->pay_info) }}
+                            {{ dd($row->pay_info) }}
                             @foreach(json_decode($row->pay_info, true) as $k => $v ) 
                                 {{ $k . ": " . $v . ' | ' }} 
                             @endforeach 
