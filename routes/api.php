8101043077571fbdf9c8f426341d65b91f1dfa30
Quang <gmqn01@gmail.com>
2023-09-27 10:11:02 +0700

    update API refunds

--- routes/api.php
+++ routes/api.php
@@ -85,9 +85,9 @@ Route::middleware(['api.user'])->group(function () {
     Route::get('/v3/child', 'Apis\MeApi@childAccountAPI');
     Route::get('/v3/class', 'Apis\MeApi@list');
 
-    Route::get('/v3/order-return', 'MeApi@deliveredOrders');
+    Route::get('/v3/order-return', 'Apis\MeApi@deliveredOrders');
 
-    Route::post('/v3/order-return/send-request/{orderId}', 'MeApi@sendReturnRequest');
+    Route::post('/v3/order-return/send-request/{orderId}', 'Apis\MeApi@sendReturnRequest');
 
     Route::get('/v3/auth/cart', 'Apis\CartApi@index');
     Route::get('/v3/auth/home', 'Apis\HomeApi@index');
