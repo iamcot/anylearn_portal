2b6b4edac5e61036261de5bf22d46bc5555e5115
Quang <gmqn01@gmail.com>
2023-10-10 16:23:35 +0700

    update

--- config/subtype_list.php
+++ config/subtype_list.php
@@ -3,9 +3,10 @@
 use App\Constants\ItemConstants;
 
  return [
-    0 => ItemConstants::SUBTYPE_DIGITAL, 
-    1 => ItemConstants::SUBTYPE_EXTRA, 
-    2 => ItemConstants::SUBTYPE_OFFLINE, 
-    3 => ItemConstants::SUBTYPE_ONLINE, 
-    4 => ItemConstants::SUBTYPE_VIDEO
+    0 => ItemConstants::SUBTYPE_DIGITAL,
+    1 => ItemConstants::SUBTYPE_EXTRA,
+    2 => ItemConstants::SUBTYPE_OFFLINE,
+    3 => ItemConstants::SUBTYPE_ONLINE,
+    4 => ItemConstants::SUBTYPE_VIDEO,
+    5 => ItemConstants::SUBTYPE_PRESCHOOL,
 ];
