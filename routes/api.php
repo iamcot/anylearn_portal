da566db82e11706592a738ce51dd8064017b1ea6
Quang <gmqn01@gmail.com>
2023-10-18 10:56:50 +0700

    update location

--- routes/api.php
+++ routes/api.php
@@ -89,6 +89,8 @@ Route::middleware(['api.user'])->group(function () {
     Route::get('/v3/getextrafee/{id}', 'Apis\MeApi@getExtrafee');
     Route::get('/v3/extrafee/{id}', 'Apis\MeApi@addExtrafee');
 
+    Route::get('/v3/locations', 'Apis\MeApi@locations');
+
     Route::get('/v3/order-return', 'Apis\MeApi@deliveredOrders');
 
     Route::get('/v3/order-return/send-request/{orderId}', 'Apis\MeApi@sendReturnRequest');
