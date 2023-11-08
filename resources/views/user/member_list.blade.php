589b51a7c5cfbe2cb4e40c988434d1b74ad46f47
Tina <ngoxuyen214@gmail.com>
2023-11-08 22:00:43 +0700

    testing

--- resources/views/user/member_list.blade.php
+++ resources/views/user/member_list.blade.php
@@ -83,7 +83,7 @@
                     <select name="sale_priority" id="sale_priority" class="form-control">
                         <option value="">---</option>
                         @foreach($priorityLevels as $key => $prio)
-                            <option value="{{ $key }}" {{ isset(Request::get('sale_priority')) && Request::get('sale_priority') == $key ? "selected" : "" }}>
+                            <option value="{{ $key }}" {{ null != Request::get('sale_priority') && Request::get('sale_priority') == $key ? "selected" : "" }}>
                                 {{ strtoupper($prio) }}
                             </option>
                         @endforeach
