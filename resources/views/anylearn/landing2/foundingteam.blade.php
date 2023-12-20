da317e5924959bc1d76777dfbe0f65e8a7c244fe
Tina <ngoxuyen214@gmail.com>
2023-12-20 15:49:07 +0700

    update content

--- resources/views/anylearn/landing2/foundingteam.blade.php
+++ resources/views/anylearn/landing2/foundingteam.blade.php
@@ -43,5 +43,45 @@
                 </div>
             </div>
         </div>
+        <div class="text-center">
+            <div class="row mt-4">
+                <div class="col-md-4">
+                    <div class="card border-0">
+                        <div class="card-body">
+                            <img src="/cdn/anylearn/img/mrThang.png" alt="">
+                            <img class="bg-img" src="/cdn/anylearn/img/mrHuyen.jpg" width="288" alt="">
+                        </div>
+                        <div class="card-footer border-0 text-center">
+                            <div class="fs-5 fw-bold">Mr. Lê Thế Huyên</div>
+                            <span class="fs-6">Legal Team Leader</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card border-0">
+                        <div class="card-body">
+                            <img src="/cdn/anylearn/img/mrThang.png" alt="">
+                            <img class="bg-img" src="/cdn/anylearn/img/msDung.jpg" width="288" alt="">
+                        </div>
+                        <div class="card-footer border-0 text-center">
+                            <div class="fs-5 fw-bold">Ms. Nguyễn Thị Viên Dung</div>
+                            <span class="fs-6">Head of Partner Relationship</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card border-0">
+                        <div class="card-body">
+                            <img src="/cdn/anylearn/img/mrThang.png" alt="">
+                            <img class="bg-img" src="/cdn/anylearn/img/msNgoc.jpg" width="288"  alt="">
+                        </div>
+                        <div class="card-footer border-0 text-center">
+                            <div class="fs-5 fw-bold">Ms. Dương Ngọc</div>
+                            <span class="fs-6">Head of Financial Accounting Team</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
--- resources/views/anylearn/landing2/number.blade.php
+++ resources/views/anylearn/landing2/number.blade.php
@@ -8,7 +8,7 @@
                             <img src="/cdn/anylearn/img/landing_number1.png" alt="" class="img-fluid w-75">
                         </div>
                         <div class=" col-sm-7 text-start ">
-                            <div class="text-success fw-bold">150+</div>
+                            <div id="num-partners" class="text-success fw-bold fs-3">500+</div>
                             <div class="text-black-50">@lang('Đối tác')</div>
                         </div>
                     </div>
@@ -20,7 +20,7 @@
                             <img src="/cdn/anylearn/img/landing_number2.png" alt="" class="img-fluid w-75">
                         </div>
                         <div class=" col-sm-7 text-start  ">
-                            <div class="text-success fw-bold">500+</div>
+                            <div id="num-courses" class="text-success fw-bold fs-3">2500+</div>
                             <div class="text-black-50">@lang('Khoá học')</div>
                         </div>
                     </div>
@@ -31,20 +31,20 @@
                         <div class="col-sm-5 pb-3">
                             <img src="/cdn/anylearn/img/landing_number3.png" alt="" class="img-fluid w-75">
                         </div>
-                        <div class=" col-sm-7  text-start ">
-                            <div class="text-success fw-bold">200+</div>
+                        <div class=" col-sm-7 text-start ">
+                            <div id="num-professors" class="text-success fw-bold fs-3">200+</div>
                             <div class="text-black-50">@lang('Chuyên gia')</div>
                         </div>
                     </div>
 
                 </div>
-                <div class="col-3 ">
+                <div class="col-3">
                     <div class="row landing-number-item">
                         <div class="col-sm-5 pb-3">
                             <img src="/cdn/anylearn/img/landing_number4.png" alt="" class="img-fluid w-75">
                         </div>
                         <div class=" col-sm-7  text-start ">
-                            <div class="text-success fw-bold">7000+</div>
+                            <div id="num-students" class="text-success fw-bold fs-3">15000+</div>
                             <div class="text-black-50">@lang('Học viên')</div>
                         </div>
                     </div>
@@ -53,4 +53,4 @@
             </div>
         </div>
     </div>
-        </section>
+</section>
