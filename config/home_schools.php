dad2f17d1ceca12f92c90ff04ecfb62949ee10d2
Tina <ngoxuyen214@gmail.com>
2024-03-07 18:36:47 +0700

    update logos

--- config/home_schools.php
+++ config/home_schools.php
@@ -66,6 +66,131 @@
     [
         'image' => '/cdn/onepage/images/schools/W-22.jpg',
     ],
-    
+    [
+        'image' => '/cdn/onepage/images/schools/W-23.png',
+    ], 
+    [
+        'image' => '/cdn/onepage/images/schools/W-24.jpg',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-25.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-26.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-27.jpg',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-28.jpg',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-29.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-30.jpg',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-31.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-32.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-33.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-34.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-35.jpg',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-37.jpeg',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-39.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-40.jpg',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-41.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-42.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-43.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-44.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-45.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-46.jpg',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-47.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-48.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-49.jpg',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-50.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-51.jpg',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-52.jpg',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-53.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-54.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-55.jpg',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-56.jpg',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-57.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-58.jpg',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-59.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-60.png',
+    ],   
+    [
+        'image' => '/cdn/onepage/images/schools/W-61.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-62.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-63.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-64.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-65.png',
+    ],
+    [
+        'image' => '/cdn/onepage/images/schools/W-66.jpg',
+    ],   
 ];
 
