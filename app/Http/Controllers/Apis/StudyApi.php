3ddc8c1d7a5fae8b044ee432df79ed2e6d7429c4
CoT <iamcot@gmail.com>
2024-05-17 15:49:02 +0700

    fix

--- app/Http/Controllers/Apis/StudyApi.php
+++ app/Http/Controllers/Apis/StudyApi.php
@@ -15,38 +15,37 @@ class StudyApi extends Controller
     private $studyServ;
     private $userServ;
 
-    public function __construct(StudyServices $studyServ, UserServices $userServ) 
+    public function __construct(StudyServices $studyServ, UserServices $userServ)
     {
         $this->studyServ = $studyServ;
         $this->userServ = $userServ;
     }
 
-    public function index(Request $request) 
+    public function index(Request $request)
     {
-        try {    
+        try {
             $data['user_info'] = $request->get('_user');
             $data['user_accounts'] = $this->userServ->accountC($data['user_info']->id);
-            
+
             if ($request->get('child')) {
                 $data['user_info'] = $data['user_accounts']->find($request->get('child')) ?? $data['user_info'];
             }
-        
+
             $data['num_items'] = $this->studyServ->getSchedulePlansForDay($data['user_info'])->count();
             $data['ongoing_items'] = $this->studyServ->getRegisteredItems($data['user_info'], ItemConstants::STATUS_ONGOING);
             $data['upcoming_items'] = $this->studyServ->getRegisteredItems($data['user_info'], ItemConstants::STATUS_UPCOMING);
             $data['completed_items'] = $this->studyServ->getRegisteredItems($data['user_info'], ItemConstants::STATUS_COMPLETED);
 
             return response()->json($data);
-
         } catch (\Exception $e) {
             return response()->json(
-                ['error' => 'Internal Server Error'], 
+                ['error' => 'Internal Server Error'],
                 HTTPConstants::HTTP_INTERNAL_SERVER_ERROR
             );
         }
     }
-    
-    public function show(Request $request, $orderItemID) 
+
+    public function show(Request $request, $orderItemID)
     {
         $user = $request->get('_user');
         $data = $this->studyServ->getRegisteredItemInfo($user, $orderItemID);
@@ -55,13 +54,13 @@ class StudyApi extends Controller
         };
         if (!empty($data->activation_info)) {
             $activationInfo = json_decode($data->activation_info, true);
-            $data->activation_info = $activationInfo !== null || json_last_error() === JSON_ERROR_NONE 
-                ? $activationInfo
+            $data->activation_info = $activationInfo != null || json_last_error() === JSON_ERROR_NONE
+                ? ['code' => 'Tài khoản: ' . $activationInfo['account'] . '\n' . 'Mật khẩu: ' . $activationInfo['password']]
                 : ['code' => $data->activation_info];
-        } 
+        }
         return response()->json($data);
     }
-    
+
     public function lookup(Request $request)
     {
         try {
@@ -73,18 +72,17 @@ class StudyApi extends Controller
             ]);
 
             $plans = $this->studyServ->getSchedulePlans($user, $data['from'], $data['to']);
-            $data['current_plans'] = array_key_exists($data['date'], $plans) 
-               ? $plans[$request->get('date')] 
-               : [];
+            $data['current_plans'] = array_key_exists($data['date'], $plans)
+                ? $plans[$request->get('date')]
+                : [];
             $data['schedule_plans'] = $plans;
 
             return response()->json($data);
-
         } catch (ValidationException $e) {
             return response()->json(
-                ['error' => 'Invalid Arguments'], 
+                ['error' => 'Invalid Arguments'],
                 HTTPConstants::HTTP_UNPROCESSABLE_ENTITY
             );
-        } 
+        }
     }
 }
